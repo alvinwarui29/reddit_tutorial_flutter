bdb63cb4c81a031a42b499885ac3fd1262bf74a9
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-22 09:11:03 +0300

    add login screen

---
+++ lib/features/auth/screens/LoginScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/core/constants/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Skip',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/features/auth/screens/LoginScreen.dart';
 import 'package:reddit_tutorial/theme/pallete.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: Pallete.darkModeAppTheme,
-      home: const SizedBox(),
+      home: const LoginScreen(),
     );
   }
 }
